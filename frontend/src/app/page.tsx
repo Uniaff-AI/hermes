38fc0b1132812df450cdf11d2f5562ebd9888a34
Yulian <hard.guzanu@gmail.com>
2025-07-17 01:41:52 +0300

    feat(architecture): restructure project without shared workspace for improved clarity and maintainability

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -16,7 +16,8 @@ import UnstyledLink from '@/components/links/UnstyledLink';
  * You can override the next-env if the type is important to you
  * @see https://stackoverflow.com/questions/68103844/how-to-override-next-js-svg-module-declaration
  */
-import Logo from '~/svg/Logo.svg';
+// @ts-ignore
+import Logo from '../../public/svg/Logo.svg';
 
 // !STARTERCONF -> Select !STARTERCONF and CMD + SHIFT + F
 // Before you begin editing, follow all comments with `STARTERCONF`,
